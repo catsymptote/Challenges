5d849d416941413ffe46bfb8b2da44c00d91fbe3
catsymptote <catsymptote@gmail.com>
2019-02-18 16:59:15 +0100

    Problem 2 complete

--- ProgrammerPlus/1/main.cpp
+++ ProgrammerPlus/1/main.cpp
@@ -3,6 +3,29 @@
 
 int main()
 {
-    std::cout << "Hello world!" << std::endl;
+    int sum = 2;
+    int num_1 = 1;
+    int num_2 = 2;
+    int term_count = 20;
+
+    std::cout << "Fibonacci sequence terms for the first " << term_count << " terms:" << std::endl;
+    std::cout << num_1 << std::endl;
+    std::cout << num_2 << std::endl;
+
+    for(int i=0; i<term_count -2; i++)
+    {
+        int num_next = num_1 + num_2;
+        num_1 = num_2;
+        num_2 = num_next;
+        std::cout << num_next << std::endl;
+
+        if(num_next %2 == 0)
+        {
+            sum += num_next;
+        }
+    }
+
+    std::cout << "\nSum of even-numbered elements:\n" << sum << std::endl;
+
     return 0;
 }
