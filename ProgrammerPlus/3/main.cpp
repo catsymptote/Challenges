ea15e90a72d0036d41ef958cd511b4ae596b4227
catsymptote <catsymptote@gmail.com>
2019-02-24 23:12:26 +0100

    is_palindromic() and flip_string() working

--- ProgrammerPlus/3/main.cpp
+++ ProgrammerPlus/3/main.cpp
@@ -1,9 +1,55 @@
 #include <iostream>
+#include <string>
+#include <vector>
 
-using namespace std;
+
+std::string flip_string(std::string str)
+{
+    std::string str_new;
+    for (int i=str.size()-1; i >= 0; i--)
+    {
+        str_new += str[i];
+    }
+    return str_new;
+}
+
+
+bool is_palindromic(int num)
+{
+    // Make an inverse/flipped num.
+    std::string num_str = std::to_string(num);
+    std::string flipped_num_str = flip_string(num_str);
+    int flipped_num = std::stoi(flipped_num_str);
+
+    // Check if nums are the same (and thus palindromic).
+    if(flipped_num == num)
+        return true;
+    return false;
+}
+
+
+std::vector<int> find_palindromic_number
+    (int digits, int num_count)
+{
+    std::vector<int> palins;
+    // for -> for ->
+
+    return palins;
+}
+
 
 int main()
 {
-    cout << "Hello world!" << endl;
+    //td::cout << "Hi" << std::endl;
+    //std::cout << flip_string("Hey There!") << std::endl;
+    if(is_palindromic(90011009))
+        std::cout << "Is palin...";
+    else
+        std::cout << "Is NOT palin...";
+    /*
+    std::vector<int> palins = find_palindromic_number(2, 2);
+    for(int i=0; i<palins.size(); i++)
+        std::cout << palins.at(i) << "   ";
+    */
     return 0;
 }
