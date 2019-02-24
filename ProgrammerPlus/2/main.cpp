4f07b76dbce58804192077e30928a53e7522216d
catsymptote <catsymptote@gmail.com>
2019-02-24 22:03:07 +0100

    Now works with multiple of the same prime
    16 = 2x2x2x2

--- ProgrammerPlus/2/main.cpp
+++ ProgrammerPlus/2/main.cpp
@@ -4,7 +4,7 @@
 
 bool is_prime(int num)
 {
-    // Check if prime
+    // Check if prime.
     bool prime = true;
     for(int i=2; i < num; i++)
         if(num%i == 0)
@@ -18,16 +18,28 @@ bool is_prime(int num)
 
 std::vector<int> find_primes(int num)
 {
-    // List of primes
+    // List of primes.
     std::vector<int> primes;
 
-    // Looping through potential primes
-    for(int i=2; i < num; i++)
+    // Add -1 if number is negative.
+    if(num < 0)
+        primes.push_back(-1);
+
+    // Looping through potential primes.
+    //for(int i=2; i < num; i++)
+    int i = 2;
+    while (i <= num)
     {
         if(num%i == 0)
+        {
             if(is_prime(i))
+            {
                 primes.push_back(i);
-
+                num /= i;
+                i = 1;
+            }
+        }
+        i++;
     }
     return primes;
 }
@@ -35,7 +47,9 @@ std::vector<int> find_primes(int num)
 
 int main()
 {
+    //int num = 600851475143;
     int num = 13195;
+    //int num = 16;
     std::vector<int> primes = find_primes(num);
     for(int i=0; i < primes.size(); i++)
     {
