6e8f61abb802b431de7271ff0271cdcef673b390
catsymptote <catsymptote@gmail.com>
2019-02-24 23:38:48 +0100

    Functional and giving solution

--- ProgrammerPlus/4/main.cpp
+++ ProgrammerPlus/4/main.cpp
@@ -1,8 +1,22 @@
 #include <iostream>
 
 
+bool divi(int counter, int nums)
+{
+    for(int i=2; i <= nums; i++)
+        if(counter%i != 0)
+            return true;
+    return false;
+}
+
+
 int main()
 {
-    std::cout << "Hello world!" << std::endl;
+    int counter = 1;
+    int nums = 20;
+    while(divi(counter, nums))
+        counter++;
+
+    std::cout << counter << std::endl;
     return 0;
 }
